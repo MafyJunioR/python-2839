f5cc2cb9fff40eb47cfb03f6b5008e4db373f76b
javier <javier@ipartekaula.com>
2025-03-20 18:31:20 +0100

    Ejemplos básicos

---
+++ basicos/hola.py
@@ -0,0 +1,28 @@
+nombre = 'Javier'
+
+print('Hola ' + nombre) # Concatenación
+
+print('El dijo: \t"Qué tal"') # Tabulador
+
+# Texto multilínea
+print('''
+El dijo: 
+'Qué tal'
+''')
+
+print("El dijo: \"Qué tal ☺️\" \\") # Uso de caracteres de emojis gracias a tener UTF-8
+
+if nombre == 'Javier':
+    print('Hombre... Si es el profe')
+    print('Otro')
+
+print('C:\\nuevos\\trabajos.txt')
+print(r'C:\nuevos\trabajos.txt')
+
+print('Hola' * 2 + ' vecinito')
+
+print('asdfasdf' + ('-' * 25))
+
+print('Una línea super larga en la que sueltas un rollo que te pasas'
+      ' y sigue y sigue y sigue sin parar hasta el infinito y más allá')
+
