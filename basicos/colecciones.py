2d7b781f1768eba5c6d7d1728b7cf836750884a2
Javier <javier@eskura21.com>
2025-03-24 20:57:12 +0100

    Cambio de colecciones a diccionarios

--- basicos/colecciones.py
+++
@@ -1,9 +0,0 @@
-coleccion = { 
-    'casa': 'house', 
-    'perro': 'dog' 
-}
-
-print(coleccion['casa'])
-
-for clave in coleccion:
-    print(clave, coleccion[clave], sep='\t')
---
+++ basicos/diccionarios.py
@@ -0,0 +1,9 @@
+diccionario = { 
+    'casa': 'house', 
+    'perro': 'dog' 
+}
+
+print(diccionario['casa'])
+
+for clave in diccionario:
+    print(clave, diccionario[clave], sep='\t')
